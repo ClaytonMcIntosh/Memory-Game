577686506d212493e55fd5930dde72f98893c699
Clay <claymcintosh@gmail.com>
2022-01-15 07:51:12 +1000

    Added colour variables, simple flex and wrap, and a bit more layout and style.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 let gameBoard = document.querySelector("#game");
 
+//loop to add 12 cards (divs) to the game
+
 for (let i = 0; i < 12; i++) {
   let card = document.createElement("div");
   card.id = `card${i}`;
