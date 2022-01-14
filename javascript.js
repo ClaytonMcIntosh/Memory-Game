57deaac542d653f833d3370cb0e158dc64ed84e8
Clay <claymcintosh@gmail.com>
2022-01-14 15:48:19 +1000

    experimenting with ways to add tiles to the board.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+let card1 = document.createElement("div");
+card1.setAttribute("class","card");
+card1.setAttribute("id","n1");
+let textNode = document.createTextNode("1");
+card1.appendChild(textNode);
+let con = document.querySelector("#game");
+con.appendChild(card1);
+
+
