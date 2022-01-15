c1aeda51cdc0c599c949f15cd8213f41c79475c1
Clay <claymcintosh@gmail.com>
2022-01-15 22:57:50 +1000

    I'm able to get only two colours, but having trouble getting all colours to show up. I'm having trouble with the loop.

--- javascript.js
+++ javascript.js
@@ -1,90 +1,120 @@
 let gameBoard = document.querySelector("#game");
+const deck = {
+  green: 2,
+  yellow: 2,
+  blue: 2,
+  red: 2,
+  purple: 2,
+  orange: 2,
+};
 
-//loop to add 12 cards (divs) to the game
+function init() {
+  addCards();
+}
 
-addCards();
-
+//Adds cards to board and calls chooseColour function to choose colour
 function addCards() {
   for (let i = 0; i < 12; i++) {
     let card = document.createElement("div");
     card.id = `card${i}`;
+
     card.setAttribute("class", chooseColor());
+
     gameBoard.appendChild(card);
+    console.log(deck);
   }
 }
 
-function chooseColor(){
+//Randomly chooses a colour
+function chooseColor() {
   let rand = Math.floor(Math.random() * 6) + 1;
-  if (rand == 1){
-  x = "green"
-  return x;
-  } else if (rand == 2){
-  x = "yellow"
-  return x;
-  }  else if (rand == 3){
-    x = "blue"
+  console.log(rand);
+  if (rand == 1 && deck.green > 0) {
+    deck.green--;
+    x = "green";
+    console.log("green=" + deck.green);
     return x;
-  }  else if (rand == 4){
-   x = "red"
-   return x;
-  } else if (rand == 5){
-    x = "purple"
+  } else if (rand == 2 && deck.yellow > 0) {
+    deck.yellow--;
+    x = "yellow";
+    console.log("yellow=" + deck.yellow);
     return x;
-   } else {
-    x = "orange"
+  } else if (rand == 3 && deck.blue > 0) {
+    deck.blue--;
+    x = "blue";
+    console.log("blue=" + deck.blue);
     return x;
-   }
-  
+  } else if (rand == 4 && deck.red > 0) {
+    deck.red--;
+    x = "red";
+    console.log("red=" + deck.red);
+    return x;
+  } else if (rand == 5 && deck.purple > 0) {
+    deck.purple--;
+    x = "purple";
+    console.log("purple=" + deck.purple);
+    return x;
+  } else if (rand == 6 && deck.orange > 0) {
+    deck.orange--;
+    x = "orange";
+    console.log("orange=" + deck.orange);
+    return x;
+  } else {
+    x = "card";
+    console.log("goal!");
+    return x;
+  }
 }
 
-// function chooseNumber() {
-//   let x = Math.floor(Math.random() * 12) + 1;
-//   return x;
-// }
+init();
 
-// const cardsObj = {
-//   0: document.getElementById("card1"),
-//   1: document.getElementById("card2"),
-//   2: document.getElementById("card3"),
-//   3: document.getElementById("card4"),
-//   4: document.getElementById("card5"),
-//   5: document.getElementById("card6"),
-//   6: document.getElementById("card7"),
-//   7: document.getElementById("card8"),
-//   8: document.getElementById("card9"),
-//   9: document.getElementById("card10"),
-//   10: document.getElementById("card11"),
-//   11: document.getElementById("card12"),
-// };
-
-// //set color
-
-// function randomNumberSix() {
-//   let x = Math.floor(Math.random() * 6) + 1;
-//   console.log("x is =" + x);
-//   return x;
-// }
-
-// generateNumbs();
-
-// function generateNumbs() {
-//   for (let i = 0; i < 12; i++) {
-//     console.log(i);
-//     if (randomNumberSix() === 1) {
-//       cardsObj[i].setAttribute("class", "green");
-
-//     } else if (randomNumberSix() === 2) {
-//       cardsObj[i].setAttribute("class", "yellow");
-//     } else if (randomNumberSix() === 3) {
-//       cardsObj[i].setAttribute("class", "blue");
-//     } else if (randomNumberSix() === 4) {
-//       cardsObj[i].setAttribute("class", "red");
-//     } else if (randomNumberSix() === 5) {
-//       cardsObj[i].setAttribute("class", "purple");
-//     } else if (randomNumberSix() === 6) {
-//       cardsObj[i].setAttribute("class", "orange");
-//     }
-//   }
-// }
-
-// // function numbText() {
+// const cardDeck = [
+//   {
+//     cardNumber: 1,
+//     cardColor: "green",
+//   },
+//   {
+//     cardNumber: 2,
+//     cardColor: "green",
+//   },
+//   {
+//     cardNumber: 3,
+//     cardColor: "yellow",
+//   },
+//   {
+//     cardNumber: 4,
+//     cardColor: "yellow",
+//   },
+//   {
+//     cardNumber: 5,
+//     cardColor: "blue",
+//   },
+//   {
+//     cardNumber: 6,
+//     cardColor: "blue",
+//   },
+//   {
+//     cardNumber: 7,
+//     cardColor: "red",
+//   },
+//   {
+//     cardNumber: 8,
+//     cardColor: "red",
+//   },
+//   {
+//     cardNumber: 9,
+//     cardColor: "purple",
+//   },
+//   {
+//     cardNumber: 10,
+//     cardColor: "purple",
+//   },
+//   {
+//     cardNumber: 11,
+//     cardColor: "orange",
+//   },
+//   {
+//     cardNumber: 12,
+//     cardColor: "orange",
+//   },
+// ];
