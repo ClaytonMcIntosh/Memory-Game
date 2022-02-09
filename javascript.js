4905fec92a854949871e6c54a8045d8816a5c2a3
Clay <claymcintosh@gmail.com>
2022-02-09 16:45:48 +1000

    change toggle to set interval

--- javascript.js
+++ javascript.js
@@ -52,103 +52,91 @@ function dealCards() {
     let cover = document.createElement("div");
     cover.id = `cover${i}`;
     cover.className = "grey";
-    cover.style.opacity = 0.8;
+    cover.style.opacity = 1;
     card.appendChild(cover);
   }
 }
 
 document.getElementById("cover0").addEventListener("click", function () {
-  if (cover0.style.opacity === "0.8") {
-    cover0.style.opacity = "0";
-  } else {
-    cover0.style.opacity = "0.8";
-  }
+  cover0.style.opacity = "0";
+  setInterval(function () {
+    cover0.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover1").addEventListener("click", function () {
-  if (cover1.style.opacity === "0.8") {
-    cover1.style.opacity = "0";
-  } else {
-    cover1.style.opacity = "0.8";
-  }
+  cover1.style.opacity = "0";
+  setInterval(function () {
+    cover1.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover2").addEventListener("click", function () {
-  if (cover2.style.opacity === "0.8") {
-    cover2.style.opacity = "0";
-  } else {
-    cover2.style.opacity = "0.8";
-  }
+  cover2.style.opacity = "0";
+  setInterval(function () {
+    cover2.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover3").addEventListener("click", function () {
-  if (cover3.style.opacity === "0.8") {
-    cover3.style.opacity = "0";
-  } else {
-    cover3.style.opacity = "0.8";
-  }
+  cover3.style.opacity = "0";
+  setInterval(function () {
+    cover3.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover4").addEventListener("click", function () {
-  if (cover4.style.opacity === "0.8") {
-    cover4.style.opacity = "0";
-  } else {
-    cover4.style.opacity = "0.8";
-  }
+  cover4.style.opacity = "0";
+  setInterval(function () {
+    cover4.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover5").addEventListener("click", function () {
-  if (cover5.style.opacity === "0.8") {
-    cover5.style.opacity = "0";
-  } else {
-    cover5.style.opacity = "0.8";
-  }
+  cover5.style.opacity = "0";
+  setInterval(function () {
+    cover5.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover6").addEventListener("click", function () {
-  if (cover6.style.opacity === "0.8") {
-    cover6.style.opacity = "0";
-  } else {
-    cover6.style.opacity = "0.8";
-  }
+  cover6.style.opacity = "0";
+  setInterval(function () {
+    cover6.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover7").addEventListener("click", function () {
-  if (cover7.style.opacity === "0.8") {
-    cover7.style.opacity = "0";
-  } else {
-    cover7.style.opacity = "0.8";
-  }
+  cover7.style.opacity = "0";
+  setInterval(function () {
+    cover7.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover8").addEventListener("click", function () {
-  if (cover8.style.opacity === "0.8") {
-    cover8.style.opacity = "0";
-  } else {
-    cover8.style.opacity = "0.8";
-  }
+  cover8.style.opacity = "0";
+  setInterval(function () {
+    cover8.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover9").addEventListener("click", function () {
-  if (cover9.style.opacity === "0.8") {
-    cover9.style.opacity = "0";
-  } else {
-    cover9.style.opacity = "0.8";
-  }
+  cover9.style.opacity = "0";
+  setInterval(function () {
+    cover9.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover10").addEventListener("click", function () {
-  if (cover10.style.opacity === "0.8") {
-    cover10.style.opacity = "0";
-  } else {
-    cover10.style.opacity = "0.8";
-  }
+  cover10.style.opacity = "0";
+  setInterval(function () {
+    cover10.style.opacity = "1";
+  }, 2000);
 });
 
 document.getElementById("cover11").addEventListener("click", function () {
-  if (cover11.style.opacity === "0.8") {
-    cover11.style.opacity = "0";
-  } else {
-    cover11.style.opacity = "0.8";
-  }
+  cover11.style.opacity = "0";
+  setInterval(function () {
+    cover11.style.opacity = "1";
+  }, 2000);
 });
