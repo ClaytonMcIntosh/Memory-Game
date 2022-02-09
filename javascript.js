6b3fb4470198598ac54ed445353e979824967ba0
Clay <claymcintosh@gmail.com>
2022-02-10 07:53:26 +1000

    attempting to get cards staying flipped if first card flipped, or both flipping back if two cards flipped

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 let gameBoard = document.querySelector("#game");
+var cardTurnNumber = false;
 const cardDeck = [
   {
     color: "green",
@@ -40,28 +41,51 @@ const cardDeck = [
 
 let randCard = cardDeck.sort(() => 0.5 - Math.random());
 
-dealCards();
+function init() {
+  dealCards();
+}
 
-//Adds cards to board and calls chooseColour function to choose colour
 function dealCards() {
   for (let i = 0; i <= 11; i++) {
     let card = document.createElement("div");
     card.id = `card${i}`;
     card.className = `${cardDeck[i].color}`;
     gameBoard.appendChild(card);
-    var cover = document.createElement("div");
-    cover.id = `cover${i}`;
-    cover.className = "grey";
-    cover.style.opacity = 1;
-    cover.addEventListener("click", flipCardBack);
-    card.appendChild(cover);
+    addCoverCard(card, i);
   }
 }
 
-function flipCardBack() {
-  var thisVar = this;
-  thisVar.style.opacity = "0";
+function addCoverCard(card, i) {
+  var cover = document.createElement("div");
+  cover.id = `cover${i}`;
+  cover.className = "grey";
+  cover.style.opacity = 1;
+  cover.addEventListener("click", checkTurnNumber);
+  card.appendChild(cover);
+}
+
+function checkTurnNumber() {
+  console.log(cardTurnNumber);
+  if (cardTurnNumber === false) {
+    cardTurnNumber = true;
+    this.style.opacity = "0";
+  } else flipCardBack(this);
+  //when you click on the first card, it stays up
+  //when you click on the second card it checks if its the same as the first card
+  //if cards are same, they go to bottom
+  //if cards are not the same, they turn back
+}
+
+// function checkIfMatch() {
+
+// }
+
+function flipCardBack(passedThis) {
+  console.log("hi");
+  let passedThisVar = passedThis;
+  passedThisVar.style.opacity = "0";
   setInterval(function () {
-    thisVar.style.opacity = "1";
+    passedThisVar.style.opacity = "1";
   }, 2000);
 }
+init();
