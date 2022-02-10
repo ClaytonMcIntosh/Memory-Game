c8a11c8d891661d8a37553ccef7d44c371fd01a5
Clay <claymcintosh@gmail.com>
2022-02-10 12:40:32 +1000

    Now able to console log match, when cards match. However code is very messy

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 let gameBoard = document.querySelector("#game");
 var cardTurnNumber = false;
 var firstCard;
+var firstCardSelected;
+var secondCardSelected;
 const cardDeck = [
   {
     color: "green",
@@ -67,10 +69,100 @@ function addCoverCard(card, i) {
 
 function checkTurnNumber() {
   if (cardTurnNumber === false) {
+    switch (this.id) {
+      case "cover0":
+        firstCardSelected = 0;
+        console.log(0);
+        break;
+      case "cover1":
+        firstCardSelected = 1;
+        break;
+      case "cover2":
+        firstCardSelected = 2;
+        break;
+      case "cover3":
+        firstCardSelected = 3;
+        break;
+      case "cover4":
+        firstCardSelected = 4;
+        break;
+      case "cover5":
+        firstCardSelected = 5;
+        break;
+      case "cover6":
+        firstCardSelected = 6;
+        break;
+      case "cover7":
+        firstCardSelected = 7;
+        break;
+      case "cover8":
+        firstCardSelected = 8;
+        break;
+      case "cover9":
+        firstCardSelected = 9;
+        break;
+      case "cover10":
+        firstCardSelected = 10;
+        break;
+      case "cover11":
+        firstCardSelected = 11;
+        break;
+      default:
+        console.log("ERR");
+        break;
+    }
     cardTurnNumber = true;
     this.style.opacity = "0";
   } else {
+    switch (this.id) {
+      case "cover0":
+        secondCardSelected = 0;
+        console.log(0);
+        break;
+      case "cover1":
+        secondCardSelected = 1;
+        break;
+      case "cover2":
+        secondCardSelected = 2;
+        break;
+      case "cover3":
+        secondCardSelected = 3;
+        break;
+      case "cover4":
+        secondCardSelected = 4;
+        break;
+      case "cover5":
+        secondCardSelected = 5;
+        break;
+      case "cover6":
+        secondCardSelected = 6;
+        break;
+      case "cover7":
+        secondCardSelected = 7;
+        break;
+      case "cover8":
+        secondCardSelected = 8;
+        break;
+      case "cover9":
+        secondCardSelected = 9;
+        break;
+      case "cover10":
+        secondCardSelected = 10;
+        break;
+      case "cover11":
+        secondCardSelected = 11;
+        break;
+      default:
+        console.log("ERR");
+        break;
+    }
+    if (
+      cardDeck[firstCardSelected].color === cardDeck[secondCardSelected].color
+    ) {
+      console.log("Match");
+    }
     flipCardBack(this);
+    cardTurnNumber = false;
   }
 }
 
@@ -78,8 +170,8 @@ function flipCardBack(passedThis) {
   passedThis.style.opacity = "0";
   setTimeout(function () {
     for (let i = 0; i < 12; i++) {
-      document.querySelector('#cover' + i).style.opacity = 1;
-  }
+      document.querySelector("#cover" + i).style.opacity = 1;
+    }
   }, 2000);
 }
 
