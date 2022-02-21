7d6dabf701e7e4ddcd04210a4bd4e27a47de0b8b
Clay <claymcintosh@gmail.com>
2022-02-21 10:44:46 +1000

    slowly fixing the code

--- javascript.js
+++ javascript.js
@@ -4,7 +4,6 @@ var cardTurnNumber = false;
 var firstCard;
 var firstCardSelected;
 var secondCardSelected;
-var coverArray = [];
 var cardArray = [];
 var wonCards = [];
 var wonCardsArray = [];
@@ -13,7 +12,7 @@ var seconds = 30;
 var timer;
 let countDown;
 var secondsDisplay = document.getElementById("secondsDisplay");
-let banana;
+let firstCardHolder;
 const cardDeck = [
   {
     color: "green",
@@ -69,7 +68,7 @@ let randCard = cardDeck.sort(() => 0.5 - Math.random());
 
 function init() {
   dealCards();
-  // dealWinningCardsHolder();
+  dealWinningCardsHolder();
   startTimer();
 }
 
@@ -94,7 +93,6 @@ function dealCards() {
     card.className = "grey";
     gameBoard.appendChild(card);
     card.addEventListener("click", checkTurnNumber)
-    // addCoverCard(card, i);
     cardArray.push(card);
   }
 }
@@ -102,28 +100,21 @@ function dealCards() {
 function checkTurnNumber() {
   if (cardTurnNumber === false) {
     firstCardSelected = this.hex
-    console.log(firstCardSelected)
-    banana = this;
+    firstCardHolder = this;
     cardTurnNumber = true;
     this.style.backgroundColor = this.hex;
-    console.log("Yo")
-    console.log(banana)
-    console.log("Yo")
-    // this.style.pointerEvents = "none";
+    this.style.pointerEvents = "none";
   } else {
     secondCardSelected = this.hex
     var secondthis = this;
-    console.log("secondThis")
-    console.log(secondthis)
-    console.log("secondThis")
     if (
       firstCardSelected === secondCardSelected
     ) {
-      console.log("match")
-      removeCards(banana, secondthis);
-      // flashBackground();
-      // addToWonCards();
+      removeCards(firstCardHolder, secondthis);
+      flashBackground();
+      addToWonCards();
     }
+    console.log(this);
     flipCardBack(this);
     cardTurnNumber = false;
   }
@@ -140,8 +131,6 @@ function dealWinningCardsHolder() {
   }
 }
 
-
-
 function addToWonCards() {
   wonCards.push(cardDeck[firstCardSelected].hex);
 
@@ -150,53 +139,33 @@ function addToWonCards() {
   }
 }
 
-function flipCardBack(passedThis) {
-  passedThis.style.backgroundColor = passedThis.hex;
+function flipCardBack(secondcardClicked) {
+  secondcardClicked.style.backgroundColor = secondcardClicked.hex;
   for (i = 0; i < 12; i++) {
-    // coverArray[i].style.pointerEvents = "none";
+    cardArray[i].style.pointerEvents = "none";
   }
   setTimeout(function () {
     for (let i = 0; i < 12; i++) {
-      // document.querySelector("#cover" + i).style.opacity = 1;
-      // if (coverArray[i].className === "grey") {
-      // //   coverArray[i].style.pointerEvents = "auto";
-      // }
+      document.querySelector("#card" + i).style.backgroundColor = "grey";
+      // if (cardArray[i].className === "grey") {
+  cardArray[i].style.pointerEvents = "auto";
+      //  }
     }
   }, 500);
 }
 
-// function flashBackground() {
-//   document.getElementById("game").style.backgroundColor =
-//     cardDeck[firstCardSelected].hex;
-//   setTimeout(() => {
-//     document.getElementById("game").style.backgroundColor = "white";
-//   }, 600);
-// }
+function flashBackground() {
+  document.getElementById("game").style.backgroundColor = firstCardSelected;
+  setTimeout(() => {
+    document.getElementById("game").style.backgroundColor = "white";
+  }, 600);
+}
 
 function removeCards(passedCard, secondPassedCard) {
   cardsWon++;
-
-  let cardo = passedCard
-
-  console.log("Scab")
-  console.log(passedCard)
-  console.log(secondPassedCard)
-  console.log("Scab")
-  
-  banana.style.opacity = "0";
+  firstCardHolder.style.opacity = "0";
   secondPassedCard.style.opacity = "0";
-
-  console.log("aert")
-  console.log(passedCard)
-  console.log(cardo)
-  console.log("aert")
-  
-  // cardArray[firstCardSelected].style.color = "none";
-  // cardArray[firstCardSelected].style.pointerEvents = "none";
-  // // cardArray[firstCardSelected].style.opacity = "0";
-  // cardArray[secondCardSelected].style.display = "none";
-  // cardArray[secondCardSelected].style.pointerEvents = "none";
-  // // cardArray[secondCardSelected].style.opacity = "0";
+  console.log(cardsWon)
   if (cardsWon === 6) {
     gameWon();
   }
@@ -205,16 +174,13 @@ function removeCards(passedCard, secondPassedCard) {
 function gameWon() {
   for (i = 0; i < 12; i++) {
     clearInterval(countDown);
-    coverArray[i].style.display = "none";
     cardArray[i].style.display = "none";
-
     document.getElementById("gameWon").style.display = "block";
   }
 }
 
 function gameLost() {
   for (i = 0; i < 12; i++) {
-    coverArray[i].style.display = "none";
     cardArray[i].style.display = "none";
     document.getElementById("gameLost").style.display = "block";
   }
